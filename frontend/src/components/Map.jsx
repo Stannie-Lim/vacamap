ee2d67ff0aa1f73b84f0d3f3c4dd02cf3c399293
Stanley Lim <stannielim@meta.com>
2024-04-02 19:43:25 -0400

    react map

--- frontend/src/components/Map.jsx
+++ frontend/src/components/Map.jsx
@@ -1,122 +1,153 @@
-import React, { useEffect, useRef, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
+import React, { useState, useMemo } from 'react';
+
+import Mapbox, { Layer, Source, Marker } from 'react-map-gl';
 
 import countries from '../assets/countries.json';
-
-import 'mapbox-gl/dist/mapbox-gl.css';
-
-mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API;
+import borders from '../assets/borders.json';
 
 const countryToCoords = (countryName) => {
   return countries.data.find(({ country }) => country.toLowerCase() === countryName.toLowerCase());
 };
 
 export const Map = () => {
-  const map = useRef(null);
-
-  const mapContainer = useRef(null);
-
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
   const [zoom, setZoom] = useState(3);
   const [selectedCountry, setSelectedCountry] = useState(null);
+  const [markers, setMarkers] = useState([]);
 
-  useEffect(() => {
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [lng, lat],
-      zoom,
+  const addMarker = ({ lat, lng }, map) => {
+    setMarkers([...markers, { id: markers.length, lat, lng }]);
+  };
+
+  const selectCountry = (map, features) => {
+    const countryName = features[0].properties.name;
+
+    const coords = countryToCoords(countryName);
+    map.flyTo({
+      center: { lat: coords.lat, lng: coords.lng },
+      zoom: 4,
     });
-  }, [map, lat, lng]);
 
-  useEffect(() => {
-    map.current.on('load', () => {
-      map.current.addSource('cbs', {  // country-boundaries-simplified
-        'type': 'geojson',
-        'data': 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson'
-      });
+    setSelectedCountry(countryName);
 
-      map.current.addLayer({
-        "id": "cf",  // country-fills
-        "type": "fill",
-        "source": "cbs",
-        "layout": {},
-        "paint": {
-          "fill-color": "#627BC1",
-          "fill-opacity": 0.5
-        }
-      });
+    map.removeLayer('cf');
+    map.removeLayer('cb');
+    map.removeLayer('cfh');
+    map.removeSource('cbs');
 
-      map.current.addLayer({
-        "id": "cb",  // country borders
-        "type": "line",
-        "source": "cbs",
-        "layout": {},
-        "paint": {
-          "line-color": 'rgb(179, 79, 207)',
-          "line-width": 2
-        }
-      });
+    const allOtherBorders = borders.features.filter(({ properties }) => properties.name !== countryName);
 
-      map.current.addLayer({
-        "id": "cfh",  // country-fills-hover",
-        "type": "fill",
-        "source": "cbs",
-        "layout": {},
-        "paint": {
-          "fill-color": "#FFFFFF",
-          "fill-opacity": 0.1,
-        },
-        "filter": ["==", "name", ""]
-      });
-
-      map.current.on("mousemove", function (e) {
-        if (map.current.getLayer('cf')) {
-          const features = map.current.queryRenderedFeatures(e.point, { layers: ["cf"] });
-
-          if (features.length) {
-            map.current.getCanvas().style.cursor = 'pointer';
-            map.current.setFilter("cfh", ["==", "name", features[0].properties.name]);
-          } else {
-            map.current.setFilter("cfh", ["==", "name", ""]);
-            map.current.getCanvas().style.cursor = '';
-          }
-        }
-      });
-
-      map.current.on("mouseout", function () {
-        map.current.getCanvas().style.cursor = 'auto';
-        map.current.setFilter("cfh", ["==", "name", ""]);
-      });
-
-      map.current.on("click", function (e) {
-        if (map.current.getLayer('cf')) {
-          const features = map.current.queryRenderedFeatures(e.point, { layers: ["cf"] });
-          if (features.length) {
-            const coords = countryToCoords(features[0].properties.name);
-            map.current.flyTo({
-              center: { lat: coords.lat, lng: coords.lng },
-              zoom: 4,
-            });
-
-            // map.current.removeLayer('cf');
-
-            // map.current.addLayer({
-            //   "id": "selected-country-fill",  // country-fills
-            //   "type": "fill",
-            //   "source": "cbs",
-            //   "layout": {},
-            //   "paint": {
-            //     "fill-color": "blue",
-            //     "fill-opacity": 0.5
-            //   }
-            // });
-          }
-        }
-      });
+    map.addSource('cbs', {
+      'type': 'geojson',
+      'data': { ...borders, features: allOtherBorders },
     });
-  }, []);
 
-  return <div ref={mapContainer} style={{ height: '100vh', width: '100vw' }} />
+    map.addLayer({
+      "id": "cf",  // country-fills
+      "type": "fill",
+      "source": "cbs",
+      "layout": {},
+      "paint": {
+        "fill-color": "#627BC1",
+        "fill-opacity": 0.5
+      }
+    });
+
+    map.addLayer({
+      "id": "cb",  // country borders
+      "type": "line",
+      "source": "cbs",
+      "layout": {},
+      "paint": {
+        "line-color": 'rgb(179, 79, 207)',
+        "line-width": 2
+      }
+    });
+
+    map.addLayer({
+      "id": "cfh",  // country-fills-hover",
+      "type": "fill",
+      "source": "cbs",
+      "layout": {},
+      "paint": {
+        "fill-color": "#FFFFFF",
+        "fill-opacity": 0.1,
+      },
+      "filter": ["==", "name", ""]
+    });
+  };
+
+  const onMapMouseMove = ({ target: map, point }) => {
+    if (map.getLayer('cf')) {
+      const features = map.queryRenderedFeatures(point, { layers: ["cf"] });
+
+      if (features.length) {
+        map.getCanvas().style.cursor = 'pointer';
+        map.setFilter("cfh", ["==", "name", features[0].properties.name]);
+      } else {
+        map.setFilter("cfh", ["==", "name", ""]);
+        map.getCanvas().style.cursor = '';
+      }
+    }
+  }
+
+  const onMapMouseOut = ({ target: map }) => {
+    map.getCanvas().style.cursor = 'auto';
+    map.setFilter("cfh", ["==", "name", ""]);
+  }
+
+  const onMapClick = (event) => {
+    const { target: map, point, lngLat } = event;
+    if (map.getLayer('cf')) {
+      const features = map.queryRenderedFeatures(point, { layers: ["cf"] });
+      if (features.length) {
+        selectCountry(map, features);
+      } else {
+        addMarker(lngLat, map);
+      }
+    }
+  };
+
+  const removeMarker = id => {
+    setMarkers(markers.filter(marker => marker.id !== id))
+  };
+
+  const moveMarker = (id, { lat, lng }) => {
+    setMarkers(markers.map(marker => marker.id === id ? { ...marker, lat, lng } : marker));
+  };
+
+  return <Mapbox
+    onMouseMove={onMapMouseMove}
+    onMouseOut={onMapMouseOut}
+    onClick={onMapClick}
+    mapboxAccessToken={import.meta.env.VITE_MAPBOX_API}
+    initialViewState={{
+      longitude: lng,
+      latitude: lat,
+      zoom
+    }}
+    style={{ width: '100vw', height: '100vh' }}
+    mapStyle="mapbox://styles/mapbox/streets-v12"
+  >
+    <Source id="cbs" type="geojson" data={borders}>
+      <Layer id="cf" type="fill" source="cbs" layout={{}} paint={{
+        "fill-color": "#627BC1",
+        "fill-opacity": 0.5
+      }} />
+      <Layer id="cb" type="line" source="cbs" layout={{}} paint={{
+        "line-color": 'rgb(179, 79, 207)',
+        "line-width": 2
+      }} />
+      <Layer id="cfh" type="fill" source="cbs" layout={{}} paint={{
+        "fill-color": "#FFFFFF",
+        "fill-opacity": 0.1,
+      }} filter={["==", "name", ""]} />
+    </Source>
+    {markers.map(({ lat, lng, id }) => (
+      <Marker draggable key={id} longitude={lng} latitude={lat} anchor="bottom" onDragEnd={({ lngLat }) => moveMarker(id, lngLat)}>
+        <img width={50} height={50} src="https://cdn4.iconfinder.com/data/icons/small-n-flat/24/map-marker-512.png" />
+      </Marker>
+    ))}
+  </Mapbox>;
 };
